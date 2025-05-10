b74062fff2bc8f6c166d0af7652261c817203cec
Philipp Trentmann <philipp@bluegardenproject.com>
2025-05-10 19:45:23 +0200

    feat: add temp middleware for testing on staging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,40 @@
-import type { NextRequest } from "next/server"
 import { updateSession } from "./utils/supabase/middleware"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const auth = request.headers.get("authorization")
+  const password = process.env.TEMP_PAGE_PASSWORD
+
+  const env = process.env.NODE_ENV
+
+  if (env === "development") {
+    return await updateSession(request)
+  }
+
+  if (!password || password === "") {
+    throw new Error("Fatal Error in Middleware. Password not set")
+  }
+
+  if (auth) {
+    const [scheme, encoded] = auth.split(" ")
+
+    if (scheme === "Basic" && typeof encoded === "string") {
+      const decoded = atob(encoded)
+      const [, pass] = decoded.split(":")
+      if (pass === password) {
+        return await updateSession(request)
+      }
+    }
+  }
+
+  return new NextResponse("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  })
+
+  // return await updateSession(request)
 }
 
 export const config = {
